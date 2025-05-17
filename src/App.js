91141eaaf5fb184b6d24da18dc5792b5b0edce64
Ashen Ruvinda <138576609+AshenRuvinda@users.noreply.github.com>
2025-05-18 04:57:16 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
 import Routes from './routes';
